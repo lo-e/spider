60122a9a6d6dad97fdc515d5d45c34f0bed24de8
loe <lo-e@outlook.com>
2022-04-10 19:30:59 +0800

    fix and update

--- dappback.py
+++ dappback.py
@@ -10,57 +10,33 @@ from time import sleep
 nft_link_re = re.compile(r'<a class="last-souvenirs-card-wrapper" href="(.*?)">')
 image_link_re = re.compile(r'<img alt=.*src="(.*?)"/>')
 nft_name_re = re.compile(r'<span class="text-wrapper text">(.*?)</span>')
-nft_main_name_re = re.compile(r'<a href="/accounts/login.*next=/catalog/(.*?)/">')
 
 # 钉钉通知
 dingtalk = DingTalkEngine()
 
 @dataclass
-class ExpoNFT(BaseSpider):
-    nft_link:str = ''               # 详情链接
-    image_link:str = ''             # 封面
-    nft_name:str = ''               # 名称
+class Dapp(BaseSpider):
+    name:str = ''                       # 详情链接
+    earn:int = 0                        # 奖励
 
 # 主程序
-def expo():
-    nft_names = []
-    name_nft_dic = {}
+def dappback():
+    dapp_names = []
+    name_dapp_dic = {}
     initing = True
 
     while True:
         # 获取数据
         sucess_ = False
-        baseUrl = 'https://expo.chikoroko.art'
+        baseUrl = 'https://dappback.com/'
         html = askUrl(url=baseUrl, parms=None, timeout=60)
 
-        print(html)
         # 解析
         if html:
             soup = BeautifulSoup(html, 'html.parser')
-            # 主NFT
-            main_items = soup.find_all('main', class_='main-content-wrapper')
-            for item in main_items:
-                item_str = str(item)
-                nft_name_list = re.findall(nft_main_name_re, item_str)
-                for nft_name in nft_name_list:
-                    data = ExpoNFT()
-                    data.nft_name = nft_name
-
-                    if data.nft_name and data.nft_name not in nft_names:
-                        nft_names.append(data.nft_name)
-                        name_nft_dic[data.nft_name] = data
-
-                        if not initing:
-                            # 有新的nft产生
-                            content = f'【Expo NFT更新】\n{data.nft_name}\n{data.nft_link}\n{data.image_link}'
-                            dingtalk.send_ding_talk(content=content)
-
-                    sucess_ = True
-
-            # 历史NFT
-            item_list = soup.find_all('a', class_='last-souvenirs-card-wrapper')
+            item_list = soup.find_all('li', class_='last-souvenirs-card-wrapper')
             for item in item_list:
-                data = ExpoNFT()
+                data = Dapp()
                 item_str = str(item)
 
                 nft_link_list = re.findall(nft_link_re, item_str)
@@ -75,32 +51,31 @@ def expo():
                 if len(nft_name_list):
                     data.nft_name = nft_name_list[0]
 
-                if data.nft_name and data.nft_name not in nft_names:
-                    nft_names.append(data.nft_name)
-                    name_nft_dic[data.nft_name] = data
+                if data.name and data.name not in dapp_names:
+                    dapp_names.append(data.name)
+                    name_dapp_dic[data.name] = data
 
                     if not initing:
-                        # 有新的nft产生
-                        content = f'【Expo NFT更新】\n{data.nft_name}\n{data.nft_link}\n{data.image_link}'
+                        # 有新的dapp产生
+                        content = f'【Dappback更新】\n{data.name}\n{data.earn}'
                         dingtalk.send_ding_talk(content=content)
 
                 sucess_ = True
 
         # 查询间隔
         if initing:
-            content = '【Expo Initing...】'
-            show_count = min(9, len(nft_names))
+            content = '【Dappback Initing...】'
+            show_count = min(9, len(dapp_names))
             for i in range(0, show_count):
-                content += f'\n{nft_names[i]}'
+                content += f'\n{dapp_names[i]}'
             dingtalk.send_ding_talk(content=content)
 
         elif not sucess_:
-            content = '【Expo Error...\nChecking!!】'
+            content = '【Dappback Error...\nChecking!!】'
             dingtalk.send_ding_talk(content=content)
 
         initing = False
         sleep(5*60)
-
 
 # 获取数据
 def askUrl(url:str, parms:dict=None, wrapped=True, timeout:int=10):
@@ -122,7 +97,9 @@ def askUrl(url:str, parms:dict=None, wrapped=True, timeout:int=10):
 
         else:
             # 封装请求头
-            request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36'}
+            request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36',
+                               'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                               'sec-ch-ua-platform': "Windows"}
             if data:
                 req = request.Request(url=url, headers=request_headers, data=data, method='POST')
             else:
@@ -137,4 +114,4 @@ def askUrl(url:str, parms:dict=None, wrapped=True, timeout:int=10):
         print(exc)
 
 if __name__ == '__main__':
-    expo()
+    dappback()
