20a7b223b3568637d0ff1237c4a4304958baea81
loe <lo-e@outlook.com>
2022-05-18 13:02:14 +0800

    update

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 // ==UserScript==
-// @name         0x
+// @name         metamask
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       loe
-// @match        https://www.baidu.com*
-// @match        https://www.google.com*
+// @match        chrome-extension://nkbihfbeogaeaoehlefnkodbefgpgknn*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @require      https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
 // ==/UserScript==
@@ -13,18 +12,7 @@
 (function () {
     console.log('start')
 
-    setTimeout(function () {
-        open('https://weibo.com')
-        confirm('yes or no')
-    }, 2000)
-
-    window.confirm = function(confirm) {
-        if (confirm == 'yes or no') {
-            console.log(confirm)
-        }
-        open('https://www.zhihu.com')
-        // return true
-    }
+    console.log('****** metamask catched ******')
 
     console.log('end')
 })()
